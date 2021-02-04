b46f8522c28097f65a1ff1ff6f3063fb5eb91c0d
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-02-04 09:43:46 +0900

    Initial Commit

---
+++ slideApp/slideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  slideAppApp.swift
+//  slideApp
+//
+//  Created by 神山駿 on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct slideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
